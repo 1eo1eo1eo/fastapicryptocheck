f4e70ad660fbc1a2e86e2f5c706beda8856edc4e
Lev <lafitel31@gmail.com>
2024-08-11 16:10:02 +1100

    add alru_cache

--- fastapicryptocheck/backend/api_import/http_client.py
+++ fastapicryptocheck/backend/api_import/http_client.py
@@ -1,5 +1,7 @@
 from aiohttp import ClientSession
 
+from async_lru import alru_cache
+
 
 class HTTPClient:
     def __init__(self, base_url: str, api_key: str):
@@ -12,12 +14,14 @@ class HTTPClient:
 
 
 class CMCHTTPClient(HTTPClient):
+    @alru_cache
     async def get_listings(self):
         async with self._session.get("/v1/cryptocurrency/listings/latest") as response:
             result = await response.json()
             return result["data"]
         
-
+        
+    @alru_cache
     async def get_currency(self, currency_id: int):
         async with self._session.get(
             "/v2/cryptocurrency/quotes/latest",
